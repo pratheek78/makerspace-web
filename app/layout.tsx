83b6b245ee7196489b4884e7b86a8ef80f6abf9c
BhendiBoi <sunnyfromsouth@gmail.com>
2024-07-15 15:38:29 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "lol",
-  description: "add later",
+  title: "CFAL MakerSpace",
+  description: "At the CFAL Makerspace, students can dive into diverse projects spanning electronics, robotics, woodworking, 3D printing, and more. Whether tackling personal projects, academic work, or extracurricular activities, the makerspace provides a nurturing environment where ideas can thrive and practical skills are honed. Emphasizing collaborative learning, CFAL Makerspace allows students to work with peers, receive mentor guidance, and join workshops and events that foster innovation and problem-solving. By promoting exploration and experimentation, the makerspace cultivates curiosity and hands-on discovery, preparing students for academic success and future STEM careers.",
 };
 
 export default function RootLayout({
